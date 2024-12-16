1a5c73e316ef1c2cd8218e00f460b74360e2fdc7
Angelica Lazaro <angelicamld@gmail.com>
2024-12-16 14:27:00 +0100

    ajout de la réponse

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,10 +9,18 @@ import "../database/checkConnection";
 // Import the Express application from ./app
 import app from "./app";
 
-// Get the port from the environment variables
+// Start the server and listen on the specified port
+
+import type { RequestHandler } from "express";
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
+
 const port = process.env.APP_PORT;
 
-// Start the server and listen on the specified port
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
